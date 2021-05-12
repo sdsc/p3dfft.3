ce4dd0c34e3ed942dd45ff7b94508c2449449a45
Dmitry Pekurovsky <dmitry@login01.expanse.sdsc.edu>
2021-05-11 22:52:43 -0700

    Reworking the makefiles to put Fortran wrapper and module inside the library

--- sample/C/Makefile.user.C
+++ sample/C/Makefile.user.C
@@ -1,12 +1,20 @@
+# Sample makefile for user programs in C
 
-GIT_DATE=2021-5-11
-GIT_VERSION=0
+# C compiler
 CC = mpicc
+# Loader/linker
 LD = $(CC)
+# FFTW library flags
 FFTW_LIB = -L$(FFTWHOME)/lib -lfftw3 -lfftw3f
-p3dfft_topdir = ../..
+# Top level directory where P3DFFT++ is installed
+p3dfft_topdir = ../../../install
+# Compiler flags
 CFLAGS = -I$(p3dfft_topdir)/include -DGIT_VERSION=$(GIT_VERSION) -DGIT_DATE=$(GIT_DATE)
-LDFLAGS = -lm -L$(p3dfft_topdir)/build -lp3dfft.3 $(FFTW_LIB)
+# Linker flags
+LDFLAGS = -lm -L$(p3dfft_topdir)/lib -lp3dfft.3 $(FFTW_LIB)
+# Ignore these two variables - they are needed for the package examples
+GIT_DATE='"2021-5-11"'
+GIT_VERSION='"0"'
 
 test3D_r2c.o: test3D_r2c.c
 	      $(CC) -c test3D_r2c.c $(CFLAGS)
